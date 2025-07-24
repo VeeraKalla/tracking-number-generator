b8930179392fd408d07239ac505fa0328d3b04a7
unknown <veeravenkatasai87@gmail.com>
2025-07-24 13:19:37 +0530

    Initial Commit

---
+++ src/main/java/com/teleport/logistics/tracking_number_generator/TrackingNumberGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.teleport.logistics.tracking_number_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingNumberGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingNumberGeneratorApplication.class, args);
+	}
+
+}
